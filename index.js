533386e3e4cadc668001c9d09886fda14fc5367f
Ravi <kkiran.ror@gmail.com>
2020-03-09 04:50:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,15 @@
      newObj[`${key}`] = value
      return newObj
   }
+
+  function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+     object[`${key}`] = value
+     return object
+  }
+
+  function updateObjectWithKeyAndValue(object, key, value){
+    var newObj = Object.assign({}, object)
+     newObj[`${key}`] = value
+     return newObj
+  }
+
