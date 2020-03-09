73dd6e4e60cd1010cf5cbd7557abd065a9ee3128
Ravi <kkiran.ror@gmail.com>
2020-03-09 04:42:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
   var object = { prop: 1 }
 
   function updateObjectWithKeyAndValue(object, key, value){
-    var newObj = object.assign({}, obj)
+    var newObj = object.assign({}, objct)
      newObj[`${key}`] = val
      return obj
   }
